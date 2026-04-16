e7ecb2a338045980e70a34d34e6b7286b840d4ad
acasamit <165587864+acasamit@users.noreply.github.com>
2026-04-17 01:00:47 +0200

    Init Window

--- include/VulkanEngine.hpp
+++ include/VulkanEngine.hpp
@@ -1,14 +1,22 @@
-#include <vulkan/vulkan.h>
-
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+
+// window size
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
 
 class VulkanEngine {
 public:
 	void run();
 
 private:
+	GLFWwindow* window;
+
+	void initWindow();
+
 	void initVulkan();
 
 	void mainLoop();
--- src/VulkanEngine.cpp
+++ src/VulkanEngine.cpp
@@ -1,9 +1,19 @@
 #include "VulkanEngine.hpp"
 
 void VulkanEngine::run() {
+	initWindow();
 	initVulkan();
 	mainLoop();
 	cleanup();
+}
+
+void VulkanEngine::initWindow() {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // we are not using opengl
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // disable resizable window
+
+	window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan - Triangle", nullptr, nullptr);
 }
 
 void VulkanEngine::initVulkan() {
@@ -11,9 +21,13 @@ void VulkanEngine::initVulkan() {
 }
 
 void VulkanEngine::mainLoop() {
-
+	while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+	}
 }
 
 void VulkanEngine::cleanup() {
+	glfwDestroyWindow(window);
 
+	glfwTerminate();
 }
